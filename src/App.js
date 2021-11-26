15163bb070f0fc2231f312cb3ef39fbb2d44367e
Maphanew <maphanew@Maphanewui-MacBook-Air.local>
2021-11-26 20:56:29 +0900

    Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Views from './views';
 import { Route, Switch } from 'react-router-dom';
@@ -8,13 +6,11 @@ import { Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
         <Router>
           <Switch>
             <Route path="/" component={Views}/>
           </Switch>
         </Router>
-      </Provider>
     </div>
   );
 }
--- src/layouts/app-layout/index.js
+++ src/layouts/app-layout/index.js
@@ -1,64 +1,64 @@
 import React from 'react';
-import { Layout, Grid } from 'antd';
-import { Switch, Route, withRouter } from "react-router-dom";
-import { connect } from 'react-redux';
-import SideNav from 'components/layout-components/SideNav';
-import TopNav from 'components/layout-components/TopNav';
-import MobileNav from 'components/layout-components/MobileNav'
-import HeaderNav from 'components/layout-components/HeaderNav';
-import PageHeader from 'components/layout-components/PageHeader';
-import Footer from 'components/layout-components/Footer';
-import AppViews from 'views/app-views';
-import navigationConfig from "configs/NavigationConfig";
-import { 
-  SIDE_NAV_WIDTH, 
-  SIDE_NAV_COLLAPSED_WIDTH,
-  NAV_TYPE_SIDE,
-  NAV_TYPE_TOP
-} from 'constants/ThemeConstant';
-import utils from 'utils';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined, CodeOutlined } from '@ant-design/icons';
 
-const { Content } = Layout;
-const { useBreakpoint } = Grid;
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
-export const AppLayout = ({ navCollapsed, navType, location }) => {
-  const currentRouteInfo = utils.getRouteInfo(navigationConfig, location.pathname)
-  const screens = utils.getBreakPoint(useBreakpoint());
-  const isMobile = !screens.includes('lg')
-  const isNavSide = navType === NAV_TYPE_SIDE
-  const isNavTop = navType === NAV_TYPE_TOP
-  const getLayoutGutter = () => {
-    if(isNavTop || isMobile) {
-      return 0
-    }
-    return navCollapsed ? SIDE_NAV_COLLAPSED_WIDTH : SIDE_NAV_WIDTH
-  }
+const AppLayout = () => {
   return (
     <Layout>
-      <HeaderNav isMobile={isMobile}/>
-      {(isNavTop && !isMobile) ? <TopNav routeInfo={currentRouteInfo}/> : null}
-      <Layout className="app-container">
-        {(isNavSide && !isMobile) ? <SideNav routeInfo={currentRouteInfo}/> : null }
-        <Layout className="app-layout" style={{paddingLeft: getLayoutGutter()}}>
-          <div className={`app-content ${isNavTop ? 'layout-top-nav' : ''}`}>
-            <PageHeader display={currentRouteInfo?.breadcrumb} title={currentRouteInfo?.title} />
-            <Content>
-              <Switch>
-                <Route path="" component={AppViews} />
-              </Switch>
-            </Content>
-          </div>
-          <Footer />
+      <Header className="header" style={{ color: "white" }}>
+      <CodeOutlined style={{ color: "orange" }}/> TanStream
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
         </Layout>
       </Layout>
-      {isMobile && <MobileNav />}
     </Layout>
   )
 }
 
-const mapStateToProps = ({ theme }) => {
-  const { navCollapsed, navType, locale } =  theme;
-  return { navCollapsed, navType, locale }
-};
-
-export default withRouter(connect(mapStateToProps)(AppLayout));
+export default AppLayout;
